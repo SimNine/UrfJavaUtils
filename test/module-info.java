da58da7f14e891eb80d663a9611c0d126e549eb7
Chris Urffer <c.urffer@live.com>
2020-12-14 22:06:32 -0500

    added pannable panel, added custom ant build file with auto-incrementing build number

---
+++ test/module-info.java
@@ -0,0 +1,16 @@
+module urf.utils {
+	exports urf.noise.module.modifier;
+	exports urf.noise.module;
+	exports urf.noise.module.combiner;
+	exports urf.noise.exception;
+	exports urf.binarystream;
+	exports urf.huffmancompression;
+	exports urf;
+	exports urf.noise.model;
+	exports urf.noise.module.source;
+	exports urf.grapher;
+	exports urf.noise;
+
+	requires java.desktop;
+	requires junit;
+}
